b90e1d083336c6d112890df78db90ea372a562fa
DenisK0s <kostenkoden89@gmail.com>
2026-02-07 07:01:16 +0200

    task2 small improvement - bounds vars are extracted into constants

--- task2.py
+++ task2.py
@@ -1,12 +1,13 @@
 import random
 
+LOWER_BOUND = 1
+UPPER_BOUND = 100
+
 def get_numbers_ticket(min: int, max:int, quantity: int) -> list[int]:
-    lower_bound = 1
-    upper_bound = 100
     nums_range_list = list(range (min, max + 1))
     is_quantity_in_range = quantity <= len(nums_range_list)
 
-    if min < lower_bound or max > upper_bound or not is_quantity_in_range:
+    if min < LOWER_BOUND or max > UPPER_BOUND or not is_quantity_in_range:
         return []
     
     unique_random_numbers = random.sample(nums_range_list, quantity)
