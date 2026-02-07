ab68f44cf01a1aadc7976fba26b17f964853a44a
DenisK0s <kostenkoden89@gmail.com>
2026-02-07 06:59:13 +0200

    task1 small improvement - date format is extracted into constant

--- task1.py
+++ task1.py
@@ -1,8 +1,10 @@
 from datetime import datetime
 
+DATE_FORMAT = "%Y-%m-%d"
+
 def get_days_from_today(date: str) -> int:
     current_dete = datetime.today()
-    given_date = datetime.strptime(date, "%Y-%m-%d")
+    given_date = datetime.strptime(date, DATE_FORMAT)
     
     delta = current_dete - given_date
 
